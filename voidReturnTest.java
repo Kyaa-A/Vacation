48e2f203e86b0c9d46c99b5ab409fd2fb78fbe7a
Kyaa-A <asnaripacalna@gmail.com>
2023-01-17 20:42:07 +0700

    init

---
+++ voidReturnTest.java
@@ -0,0 +1,14 @@
+public class voidReturnTest {
+
+    public static void main(String[] args) {
+        System.out.println("Total: " + add(21, 34));
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+/*     public static void add(int x, int y) {
+        System.out.println(x + y);
+    } */
+}
